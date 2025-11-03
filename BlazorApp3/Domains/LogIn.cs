d5624d4c7703e080c6a037380d3bd6845de8da77
Ancedi <patrickhombrebueno@gmail.com>
2025-11-03 14:07:49 +0100

    Added Login Interface and Login Domain

---
+++ BlazorApp3/Domains/LogIn.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApp3.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp3.Domains
+{
+    public class LogIn : ILogIn
+    {
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public List<BankAccount> BankAccounts;
+
+        public LogIn (string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        [JsonConstructor]
+        public LogIn (string username, string password, List<BankAccount> bankaccounts)
+        {
+            Username = username;
+            Password = password;
+            this.BankAccounts = bankaccounts;
+        }
+    }
+}
---
+++ BlazorApp3/Interfaces/ILogIn.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp3.Interfaces
+{
+    public interface ILogIn
+    {
+        string Username { get; }
+        string Password { get; }
+    }
+}
