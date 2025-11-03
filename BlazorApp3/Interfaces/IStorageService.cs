a50cea32b7695899a864f8142ecaf8b0d40496a3
Ancedi <patrickhombrebueno@gmail.com>
2025-11-03 10:29:04 +0100

    Increased readability in select areas.

--- BlazorApp3/Interfaces/IStorageService.cs
+++ BlazorApp3/Interfaces/IStorageService.cs
@@ -5,8 +5,10 @@
     {
         //Save
         Task AddItem<T>(string key, T value);
+
         //Retrieve
         Task<T?> GetItem<T>(string key);
+
         //Delete
         Task RemoveItem<T>(string key, T value);
     }
